804d06ae4e4d8c1ccebbc6ef6d9ddcb1e51d69ad
hai <hai.nguyen7792@gmail.com>
2020-01-03 17:04:56 +0700

    Initial commit

---
+++ app/src/main/java/vn/edu/csc/jetpackpaginglibraryapp/single_movie_detail/SingleMovieActivity.kt
@@ -0,0 +1,13 @@
+package vn.edu.csc.jetpackpaginglibraryapp.single_movie_detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import vn.edu.csc.jetpackpaginglibraryapp.R
+
+class SingleMovieActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_movie)
+    }
+}
